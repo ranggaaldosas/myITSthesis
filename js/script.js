512942a022899106fbb29d47c0b510430b37b09a
rangga <ranggaaldosas@gmail.com>
2023-03-14 10:59:46 +0700

    initialize commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const themeToggleBtn = document.getElementById("theme-toggle");
+const themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+const themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+
+if (
+  localStorage.getItem("color-theme") === "dark" ||
+  (!("color-theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  // Show light icon
+  themeToggleLightIcon.classList.remove("hidden");
+} else {
+  themeToggleDarkIcon.classList.remove("hidden");
+}
+
+// Listen toggle button click
+themeToggleBtn.addEventListener("click", toggleMode);
+
+function toggleMode() {
+  // Toggle icon
+  themeToggleDarkIcon.classList.toggle("hidden");
+  themeToggleLightIcon.classList.toggle("hidden");
+
+  // Untuk menyimpan data di localstorage
+  if (localStorage.getItem("color-theme")) {
+    // Jika light, buat jadi dark trs save di localstorage
+    if (localStorage.getItem("color-theme") === "light") {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    }
+  } else {
+    // Jika gaada di localstorage
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    }
+  }
+}
---
+++ readme.md
@@ -0,0 +1,17 @@
+## Cara Run
+
+Install dependencies
+
+```
+npm Install
+```
+
+Run Tailwind CLI
+
+```
+npm run watch
+```
+
+## Screenshots
+
+![ScreenShot](https://ibb.co/nQxDRkW)
